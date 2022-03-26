6d4af469431356890348654fc093bfb0ab9986a3
minju0213 <mjni1214@naver.com>
2022-03-26 23:03:55 +0900

    03.26 - 갈아엎음

--- app/src/main/java/com/example/cooknote/MainActivity.java
+++ app/src/main/java/com/example/cooknote/MainActivity.java
@@ -1,27 +1,18 @@
 package com.example.cooknote;
 
+import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
+
 
 import android.content.Intent;
 import android.net.Uri;
 
-import android.os.Bundle;
-import android.view.View;
 
-import me.relex.circleindicator.CircleIndicator3;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private ViewPager2 mPager;
-    private FragmentStateAdapter pagerAdapter;
-    private int num_page = 3;
-    private CircleIndicator3 mIndicator;
-
-
-
 
 
     @Override
@@ -29,53 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //ViewPager2
-        mPager = findViewById(R.id.viewpager);
-        //Adapter
-        pagerAdapter = new Adapter(this, num_page);
-        mPager.setAdapter(pagerAdapter);
-        //Indicator
-        mIndicator = findViewById(R.id.indicator);
-        mIndicator.setViewPager(mPager);
-        mIndicator.createIndicators(num_page,0);
-
-        //ViewPager Setting
-        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
-        mPager.setCurrentItem(999);
-        mPager.setOffscreenPageLimit(3);
-
-        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-                if (positionOffsetPixels == 0) {
-                    mPager.setCurrentItem(position);
-                }
-            }
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                mIndicator.animatePageSelected(position%num_page);
-            }
-
-        });
-
-        final float pageMargin= getResources().getDimensionPixelOffset(R.dimen.pageMargin);
-        final float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
-
-        mPager.setPageTransformer(new ViewPager2.PageTransformer() {
-
-            public void transformPage(View page, float position) {
-                float myOffset = position * -(2 * pageOffset + pageMargin);
-                if (mPager.getOrientation() == ViewPager2.ORIENTATION_HORIZONTAL) {
-                    if (ViewCompat.getLayoutDirection(mPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
-                        page.setTranslationX(-myOffset);
-                    } else {
-                        page.setTranslationX(myOffset);
-                    }
-                } else {
-                    page.setTranslationY(myOffset);
-                }
-            }
-        });
 
     }
     // id:onButton 눌렀을 때 자격증 사이트로 연결
@@ -83,4 +27,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.q-net.or.kr/man001.do?imYn=Y&gSite=Q"));
         startActivity(intent);
     }
+
+
 }
