74085c4fe0a939979d3bf646470b45ddc2ff91e3
minju0213 <mjni1214@naver.com>
2022-04-05 21:02:56 +0900

    04.05 - 메인화면 버튼들 클릭시 각각 다른 엑티비티 연결 했음

---
+++ app/src/main/java/com/example/cooknote/Date_go.java
@@ -0,0 +1,15 @@
+package com.example.cooknote;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Date_go extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.date_go);
+    }
+}
+
--- app/src/main/java/com/example/cooknote/MainActivity.java
+++ app/src/main/java/com/example/cooknote/MainActivity.java
@@ -7,9 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
-
-
-
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +18,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //기능사 시험 날짜 보러가기 버튼 클릭시 액티비티 전환
+        ImageButton DateGo_bt = (ImageButton) findViewById(R.id.DateGO_bt);
+        //종목별 문제 풀기 버튼 클릭시 액티비티 전환
+        ImageButton Question_bt = (ImageButton) findViewById(R.id.Question_bt);
+        //종목별 레시피 보기 버튼 클릭시 액티비티 전환
+        ImageButton Recipe_bt = (ImageButton) findViewById(R.id.Recipe_bt);
+
+        //종목별 레시피 보기 버튼 클릭시 액티비티 전환
+        Recipe_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Recipe.class);
+                startActivity(intent);
+            }
+        });
+
+        //종목별 문제 풀기 버튼 클릭시 액티비티 전환
+        Question_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Question.class);
+                startActivity(intent);
+            }
+        });
+
+        //기능사 시험 날짜 보러가기 버튼 클릭시 액티비티 전환
+        DateGo_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Date_go.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
