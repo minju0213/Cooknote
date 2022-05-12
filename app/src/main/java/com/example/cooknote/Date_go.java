625b563cf764c25aebaa95eb6fb27a23019e9104
minju0213 <mjni1214@naver.com>
2022-05-12 17:28:55 +0900

    05.12 - 달력 만듦 - 메인 화면 버튼 위치 조정

--- app/src/main/java/com/example/cooknote/Date_go.java
+++ app/src/main/java/com/example/cooknote/Date_go.java
@@ -1,15 +1,35 @@
 package com.example.cooknote;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 public class Date_go extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.date_go);
+
+        CalendarView calendarView = (CalendarView)findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int datOfMonth) {
+
+                Toast.makeText(Date_go.this, "" + year + "/" + (month+1) + "/" + datOfMonth, 0).show();
+            }
+        });
+
+
     }
 }
 
