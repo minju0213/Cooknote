29499606c675e00858c0f71a8ff853d827d3adc5
jinseon901 <yanggi200@gmail.com>
2022-05-12 18:16:51 +0900

    5.12 - 제빵 누르면 엑티비티 전환되는 기능 만듦

--- app/src/main/java/com/example/cooknote/Question.java
+++ app/src/main/java/com/example/cooknote/Question.java
@@ -1,6 +1,9 @@
 package com.example.cooknote;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,15 @@ public class Question extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question);
 
+        //종목별 문제 풀기-제빵 버튼을 누를 시 엑티비티 전환
+        ImageButton bread_bt = (ImageButton) findViewById(R.id.bread_bt);
 
+        bread_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), bread.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cooknote/bread.java
+++ app/src/main/java/com/example/cooknote/bread.java
@@ -1,4 +1,14 @@
 package com.example.cooknote;
 
-public class bread {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class bread extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bread);
+    }
 }
