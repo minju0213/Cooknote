82e6b66f420aff37b83d63eb97d17f8e6cba4252
minju0213 <mjni1214@naver.com>
2022-05-12 18:42:34 +0900

    05.12 (2) - 제과.한식 액티비티 연결 - 퀘스찬 액티비티 버튼 위치 수정

---
+++ app/src/main/java/com/example/cooknote/Korean_food.java
@@ -0,0 +1,15 @@
+package com.example.cooknote;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Korean_food extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.korean_food);
+    }
+}
--- app/src/main/java/com/example/cooknote/Question.java
+++ app/src/main/java/com/example/cooknote/Question.java
@@ -14,8 +14,29 @@ public class Question extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question);
 
-        //종목별 문제 풀기-제빵 버튼을 누를 시 엑티비티 전환
+        // 제과 버튼 클릭시 제과 문제풀기 화면으로 이동
+        ImageButton cookie_bt = (ImageButton) findViewById(R.id.cookie_bt);
+        // 제빵 버튼을 클릭 시 제빵 문제풀기 화면으로 전환
         ImageButton bread_bt = (ImageButton) findViewById(R.id.bread_bt);
+        // 한식 버튼을 클릭시 제과 문제풀기 화면으로 이동
+        ImageButton korean_food_bt = (ImageButton) findViewById(R.id.korean_food_bt);
+
+
+        korean_food_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Korean_food.class);
+                startActivity(intent);
+            }
+        });
+
+        cookie_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Cookie.class);
+                startActivity(intent);
+            }
+        });
 
         bread_bt.setOnClickListener(new View.OnClickListener() {
             @Override
