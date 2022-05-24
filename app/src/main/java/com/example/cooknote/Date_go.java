de8fb08c408242f941f2339e6c6d4d44d6af6ff5
minju0213 <mjni1214@naver.com>
2022-05-24 17:03:11 +0900

    05.24 - 달력 오류 수정

--- app/src/main/java/com/example/cooknote/Date_go.java
+++ app/src/main/java/com/example/cooknote/Date_go.java
@@ -1,24 +1,23 @@
 package com.example.cooknote;
 
 import android.os.Bundle;
-import android.widget.CalendarView;
-import android.widget.Toast;
 
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 
 public class Date_go extends AppCompatActivity {
 
+    private MaterialCalendarView calendarView;
+
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.date_go);
 
+        calendarView = findViewById(R.id.calendarview);
 
 
 
